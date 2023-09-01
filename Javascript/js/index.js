260093c7f665326840ea2194a84b08238c6b0775
seb99seb <sebvangpedersen@hotmail.dk>
2023-09-01 09:40:43 +0200

    Basis for website + simple graphs

--- Javascript/js/index.js
+++ Javascript/js/index.js
@@ -1,26 +1,65 @@
 const app = Vue.createApp({
     data() {
         return {
-            word: "ok",
-            words: [],
-            Message: ""
+            Frontpage: true,
+            Page1: false,
+            Page2: false,
+            Userpage: false,
+            ElmålerIds: [1, 2, 3],
+            ElmålerVærdier: [56, 34, 19],
+            PieData: [],
+            ElPriser: [2.25, 2.1, 1.9, 2.06, 2.2],
+            Timer: ["10PM", "11PM", "12PM", "1AM", "2AM"],
+            LineData: [],
         }
     },
-    computed:{
-        wordDisplay(){
-            return `${this.words+" "}`
-        }
+    created(){
+        this.PieData.push(
+            {
+                labels: this.ElmålerIds,
+                values: this.ElmålerVærdier,
+                type: "pie"
+            }
+        )
+        this.LineData.push(
+            {
+                x: this.Timer,
+                y: this.ElPriser,
+                mode: "lines",
+                type: "scatter"
+            }
+        )
     },
     methods: {
-        // add here
-        Save(){
-            this.words.push(this.word)
+        IntoFrontpage(){
+            this.Frontpage = true,
+            this.Page1 = false,
+            this.Page2 = false,
+            this.Userpage = false
         },
-        Show(){
-            //ok
+        IntoPage1(){
+            this.Frontpage = false,
+            this.Page1 = true,
+            this.Page2 = false,
+            this.Userpage = false
         },
-        Clear(){
-            this.words=[]
+        IntoPage2(){
+            this.Frontpage = false,
+            this.Page1 = false,
+            this.Page2 = true,
+            this.Userpage = false
+        },
+        IntoUserpage(){
+            this.Frontpage = false,
+            this.Page1 = false,
+            this.Page2 = false,
+            this.Userpage = true
+        },
+        Pie(){
+            Plotly.newPlot("pieChart", this.PieData)
+        },
+        Line(){
+            Plotly.newPlot("lineChart", this.LineData)
         }
     }
 })
